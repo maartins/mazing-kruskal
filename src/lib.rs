51b589581aa4d7afd0788bd160defd02fef59d1b
Martins Dzirko <dzirko2@gmail.com>
2020-03-07 09:52:18 +0200

    Implement Cell

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::fmt;
 use rand::Rng;
 
 const WALL: u8 = 1;
@@ -37,8 +38,52 @@ impl Config {
     }
 }
 
+#[derive(Clone, Debug)]
+struct Cell {
+    pub x: usize,
+    pub y: usize,
+    pub cell_type: u8,
+    pub id: String,
+}
+
+impl Cell {
+    fn new(x: usize, y: usize, cell_type: u8, id: String) -> Cell {
+        Cell {
+            x,
+            y,
+            cell_type,
+            id
+        }
+    }
+}
+
+impl fmt::Display for Cell {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.id != "z".to_string() {
+            write!(f, "{}", self.id)
+        } else {
+            write!(f, "#")
+        }
+    }
+}
+
 pub fn run (config: Config) -> Result<(), Box<dyn Error>>{
-    let mut map = vec![vec![GROUND; config.size]; config.size];
+    let mut map = vec![vec![Cell::new(0, 0, GROUND, "z".to_string()); config.size]; config.size];
+
+    for x in 0..config.size {
+        for y in 0..config.size {
+            map[x][y].x = x;
+            map[x][y].y = y;
+
+            if x & 1 == 1 || y & 1 == 1 {
+                map[x][y].cell_type = WALL;
+                map[x][y].id = "z".to_string();
+            } else {
+                map[x][y].cell_type = GROUND;
+                map[x][y].id = "a".to_string();
+            }
+        }
+    }
 
     for (_, row) in map.iter().enumerate() {
         for (_, col) in row.iter().enumerate() {
